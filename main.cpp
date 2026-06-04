93562782e67d8e405a33a56c059d15e2f93cdb93
Agustin-malecki <maleckiagustin414@gmail.com>
2026-06-04 08:26:47 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ using namespace std;
 struct Paquete {
     int id;
     int prioridad;
-    std::string fechaCreacion;
+    std::chrono::system_clock::time_point fechaDeCreacion;
 };
 
-std::queue<Paquete> bufer1;//estanteria
-std::queue<Paquete> bufer2;//cinta
+std::queue<Paquete> buffer1;//estanteria
+std::queue<Paquete> buffer2;//cinta
 
 Semaforo espacioEnCinta;
 Semaforo paquetesEnEstanteria;
@@ -27,6 +27,58 @@ std::mutex mtxConsola;
 
 int pedido = 5;
 int siguienteId = 1;
+
+Semaforo hayDatosBuffer1; //waitingQueue
+Semaforo HayEspacioBuffer1; //waitingQueue
+Semaforo hayDatosBuffer2; //Processing Queue
+Semaforo HayEspacioBuffer2; //Processing Queue
+
+
+void productor(int cantidad) //Guardamos los paquetes en el buffer 1
+{
+    for (int i = 0; i < cantidad; i++)
+    {
+        Paquete p;
+
+        p.id = siguienteId++;
+        p.prioridad = rand() % 2;
+        p.fechaDeCreacion = chrono::system_clock::now(); //Guardamos la fecha de hoy
+        
+        wait(HayEspacioBuffer1);
+        mtx_bufer1.lock();
+        buffer1.push(p);
+        mtx_bufer1.unlock();
+
+        signal(hayDatosBuffer1);
+
+        this_thread::sleep_for(chrono::milliseconds(90));
+    }
+}
+
+void consumidor(int cant) //Saca los paquetes del buffer 2
+{
+    for (int x = 0; x<cant; x++)
+    {
+        wait(hayDatosBuffer2);
+
+        Paquete p;
+
+        mtx_bufer2.lock();
+        p = buffer2.front(); //primer elementos
+        buffer2.pop();
+        mtx_bufer2.unlock();
+
+        signal(HayEspacioBuffer2);
+
+        mtx_salida.lock();
+
+        time_t t = chrono::system_clock::to_time_t(p.fechaDeCreacion);
+
+        cout << "ID: " << p.id << " Prioridad: " << p.prioridad << " Fecha: " << ctime(&t); // lo pasa a un texto legible
+
+        mtx_salida.unlock();
+    }
+}
 
 void generarPaquete(){
 
